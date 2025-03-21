3203a9926da407fa2e1ac3466a086a11ffde5936
Rajeswari341 <rajeswarichalla388@gmail.com>
2025-03-21 18:24:06 +0530

    First Commit

---
+++ src/test/java/MyRunner/LoginStepsRunner.java
@@ -0,0 +1,13 @@
+package MyRunner;
+ 
+import org.junit.runner.RunWith;
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+ 
+@RunWith(Cucumber.class)
+@CucumberOptions(
+	features=".\\src\\test\\java\\Features\\LoginPage.feature",
+	glue="StepDef", plugin= {"pretty"},monochrome=true)
+ 
+public class LoginStepsRunner {
+}
